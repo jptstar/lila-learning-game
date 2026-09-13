87bf8477dd7b8289f66494f27e6083af2ce4927e
JP <dev@jptstar.com>
2026-09-13 17:08:47 +0200

    Register new educational games

--- games/manifest.js
+++ games/manifest.js
@@ -1,13 +1,19 @@
-// Registre des plugins de jeu.
-// Pour ajouter un nouveau jeu :
-// 1. créer games/mon-jeu/plugin.js
-// 2. l'importer ici
-// 3. l'ajouter dans GAMES
 import {discoverGame} from "./discover/plugin.js";
 import {findImageGame} from "./find-image/plugin.js";
 import {findLetterGame} from "./find-letter/plugin.js";
 import {countGame} from "./count/plugin.js";
 import {recognizeNumberGame} from "./recognize-number/plugin.js";
+import {shapesGame} from "./shapes/plugin.js";
+import {shadowsGame} from "./shadows/plugin.js";
+import {shapeHoleGame} from "./shape-hole/plugin.js";
+import {shapeRotationGame} from "./shape-rotation/plugin.js";
+import {genderGame} from "./gender/plugin.js";
+import {tracingGame} from "./tracing/plugin.js";
 import {smartGame} from "./smart/plugin.js";
 
-export const GAMES=[discoverGame,findImageGame,findLetterGame,countGame,recognizeNumberGame,smartGame];
+export const GAMES=[
+  discoverGame,findImageGame,findLetterGame,tracingGame,
+  countGame,recognizeNumberGame,
+  shapesGame,shadowsGame,shapeHoleGame,shapeRotationGame,
+  genderGame,smartGame
+];
