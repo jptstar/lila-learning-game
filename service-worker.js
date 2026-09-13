087477a590b5253c382cd6216a659a8b65a44a80
JP <dev@jptstar.com>
2026-09-13 19:13:21 +0200

    Refresh cache for adaptive counting layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="lila-v16";
+const CACHE="lila-v17";
 const FILES=[
   "./","./index.html","./manifest.webmanifest",
   "./assets/css/app.css","./assets/css/mobile-fit.css","./assets/css/mobile-device.css","./assets/css/games-extra.css",
