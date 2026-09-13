41d66e0bd41a3e1ed9655c99ebed7a0037c574c4
JP <dev@jptstar.com>
2026-09-13 15:48:37 +0200

    Refresh offline cache for number learning update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="lila-v1";
+const CACHE="lila-v2";
 const FILES=[
   "./","./index.html","./manifest.webmanifest","./assets/css/app.css",
   "./assets/js/app.js","./assets/js/data.js","./assets/js/utils.js","./assets/js/voice.js",
