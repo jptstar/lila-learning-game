59d4e63032e221e163167dbb11e31adfce35584a
JP <dev@jptstar.com>
2026-09-13 15:39:58 +0200

    Add learning data

---
+++ assets/js/data.js
@@ -0,0 +1,39 @@
+export const DATA = [
+ {l:"A",lower:"a",word:"avion",emoji:"✈️",article:"un",phrase:"A comme avion"},
+ {l:"B",lower:"b",word:"ballon",emoji:"🎈",article:"un",phrase:"B comme ballon"},
+ {l:"C",lower:"c",word:"chat",emoji:"🐱",article:"un",phrase:"C comme chat"},
+ {l:"D",lower:"d",word:"dinosaure",emoji:"🦖",article:"un",phrase:"D comme dinosaure"},
+ {l:"E",lower:"e",word:"étoile",emoji:"⭐",article:"une",phrase:"E comme étoile"},
+ {l:"F",lower:"f",word:"fleur",emoji:"🌸",article:"une",phrase:"F comme fleur"},
+ {l:"G",lower:"g",word:"girafe",emoji:"🦒",article:"une",phrase:"G comme girafe"},
+ {l:"H",lower:"h",word:"hibou",emoji:"🦉",article:"un",phrase:"H comme hibou"},
+ {l:"I",lower:"i",word:"île",emoji:"🏝️",article:"une",phrase:"I comme île"},
+ {l:"J",lower:"j",word:"jus",emoji:"🧃",article:"un",phrase:"J comme jus"},
+ {l:"K",lower:"k",word:"kiwi",emoji:"🥝",article:"un",phrase:"K comme kiwi"},
+ {l:"L",lower:"l",word:"lion",emoji:"🦁",article:"un",phrase:"L comme lion"},
+ {l:"M",lower:"m",word:"maison",emoji:"🏠",article:"une",phrase:"M comme maison"},
+ {l:"N",lower:"n",word:"nuage",emoji:"☁️",article:"un",phrase:"N comme nuage"},
+ {l:"O",lower:"o",word:"orange",emoji:"🍊",article:"une",phrase:"O comme orange"},
+ {l:"P",lower:"p",word:"poisson",emoji:"🐟",article:"un",phrase:"P comme poisson"},
+ {l:"Q",lower:"q",word:"quille",emoji:"🎳",article:"une",phrase:"Q comme quille"},
+ {l:"R",lower:"r",word:"robot",emoji:"🤖",article:"un",phrase:"R comme robot"},
+ {l:"S",lower:"s",word:"soleil",emoji:"☀️",article:"un",phrase:"S comme soleil"},
+ {l:"T",lower:"t",word:"tortue",emoji:"🐢",article:"une",phrase:"T comme tortue"},
+ {l:"U",lower:"u",word:"usine",emoji:"🏭",article:"une",phrase:"U comme usine"},
+ {l:"V",lower:"v",word:"vélo",emoji:"🚲",article:"un",phrase:"V comme vélo"},
+ {l:"W",lower:"w",word:"wagon",emoji:"🚃",article:"un",phrase:"W comme wagon"},
+ {l:"X",lower:"x",word:"xylophone",emoji:"🎶",article:"un",phrase:"X comme xylophone"},
+ {l:"Y",lower:"y",word:"yaourt",emoji:"🥣",article:"un",phrase:"Y comme yaourt"},
+ {l:"Z",lower:"z",word:"zèbre",emoji:"🦓",article:"un",phrase:"Z comme zèbre"}
+];
+
+export const COUNT_EMOJIS = [
+ {emoji:"🍎",name:"pommes"},{emoji:"⭐",name:"étoiles"},{emoji:"🐥",name:"poussins"},
+ {emoji:"🚗",name:"voitures"},{emoji:"🍓",name:"fraises"},{emoji:"⚽",name:"ballons"},
+ {emoji:"🌼",name:"fleurs"},{emoji:"🦋",name:"papillons"}
+];
+
+export const NUMBER_WORDS = {
+  1:"un",2:"deux",3:"trois",4:"quatre",5:"cinq",6:"six",7:"sept",8:"huit",9:"neuf",10:"dix",
+  11:"onze",12:"douze",13:"treize",14:"quatorze",15:"quinze",16:"seize",17:"dix-sept",18:"dix-huit",19:"dix-neuf",20:"vingt"
+};
