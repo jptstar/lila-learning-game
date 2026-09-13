9179868ad63b6668740071666a6c646ec30aea9d
JP <dev@jptstar.com>
2026-09-13 19:00:43 +0200

    Refresh cache for child touch improvements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="lila-v15";
+const CACHE="lila-v16";
 const FILES=[
   "./","./index.html","./manifest.webmanifest",
   "./assets/css/app.css","./assets/css/mobile-fit.css","./assets/css/mobile-device.css","./assets/css/games-extra.css",
